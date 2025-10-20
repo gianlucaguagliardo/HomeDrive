7d19329c48b858bc2d179321efe683e3076df6bf
HaoshokuRed <80910743+gianu12g@users.noreply.github.com>
2025-10-20 15:51:34 -0400

    Init

--- src/main/java/org/example/homedrive/HomeDriveApplication.java
+++ src/main/java/org/example/homedrive/HomeDriveApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeDriveApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HomeDriveApplication.class, args);
+        SpringApplication.run(HomeDriveApplication.class, args); //hi
     }
 }
