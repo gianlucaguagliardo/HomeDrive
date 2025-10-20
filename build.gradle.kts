56bc254280181c9831727799dcec33b89aa92b71
HaoshokuRed <80910743+gianu12g@users.noreply.github.com>
2025-10-20 15:34:37 -0400

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example"
+version = "0.1.0"
+
+java {
+    toolchain { languageVersion.set(JavaLanguageVersion.of(21)) }
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.slf4j:slf4j-api:2.0.13")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test { useJUnitPlatform() }
---
+++ src/main/java/org/example/homedrive/HomeDriveApplication.java
@@ -0,0 +1,11 @@
+package org.example.homedrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDriveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeDriveApplication.class, args);
+    }
+}
